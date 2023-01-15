3d41c7f367f28f94a6f1f969f0b80f0780de6ae8
Harsh <soni.hs661@gmail.comm>
2023-01-16 00:06:00 +0530

    first question done

---
+++ firstquestion.py
@@ -0,0 +1,21 @@
+import csv
+import os
+import sys 
+s=set()
+file = open(os.path.join(sys.path[0], "first.csv"), mode ='r')
+csvreader=csv.reader(file)
+
+#for list we already get list
+list=[]
+s=set()
+dict={}
+for row in csvreader:
+    #print(row)
+    list.append(row)
+    s.add(tuple(row))
+    dict[row[0]+" "+row[1]]=row[2]
+print("the list",list)
+print("The set ",s)
+print("The dictionary ",dict)
+    
+
