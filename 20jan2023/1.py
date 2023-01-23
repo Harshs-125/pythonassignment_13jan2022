1f64bab9f22a619d06310da06e25b6735df1e55c
Harsh <soni.hs661@gmail.comm>
2023-01-23 10:50:54 +0530

    20 jan question 4

--- 20jan2023/1.py
+++ 20jan2023/1.py
@@ -19,29 +19,38 @@ class Cow(Animal):
         self.t=t
         self.c=c
         self.l=l
+    def sound(self):
+        print("mooo sound")
 class Dog(Animal):
     def __init__(self,t,c,l):
         self.t=t
         self.c=c
         self.l=l
+    def sound(self):
+        print("barking sound")
 class Python(Animal):
     def __init__(self,t,c,l):
         self.t=t
         self.c=c
         self.l=l
+    def sound(self):
+        print("hissing sound ")
 cow=Cow('mammal','herbivorous','4')
 cow.type()
 cow.characteristic()
 cow.limbs()
+cow.sound()
 
 dog=Dog('mammal','omnivorous','4')
 dog.type()
 dog.characteristic()
 dog.limbs()
+dog.sound()
 
 python=Python('reptile','carnivorous','0')
 python.type()
 python.characteristic()
 python.limbs()
+python.sound()
 
 
